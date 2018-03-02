5efae53f4f28257e5c3e8ff3fb5b487f36b3a168
Angel Borroy <angel.borroy@keensoft.es>
2018-03-02 08:28:28 +0100

    Sorting pending rides by criteria

--- src/main/java/es/keensoft/main/StepEngine.java
+++ src/main/java/es/keensoft/main/StepEngine.java
@@ -1,9 +1,12 @@
 package es.keensoft.main;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.SortedSet;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 import es.keensoft.bean.Input;
 import es.keensoft.bean.InputLine;
@@ -120,10 +123,27 @@ public class StepEngine {
 			if (input.getRide().get(ride).getFinishingTime() <= (distance + step)) {
 				pendingScores.put(ride, 0);
 			}
-		}		
+		}
 		
+		// Order by criteria
+		entriesSortedByValues(pendingScores);
 		return new ArrayList<Integer>(pendingScores.keySet());
+		
 	}
+	
+	static <K,V extends Comparable<? super V>>
+	SortedSet<Map.Entry<K,V>> entriesSortedByValues(Map<K,V> map) {
+	    SortedSet<Map.Entry<K,V>> sortedEntries = new TreeSet<Map.Entry<K,V>>(
+	        new Comparator<Map.Entry<K,V>>() {
+	            @Override public int compare(Map.Entry<K,V> e1, Map.Entry<K,V> e2) {
+	                int res = e1.getValue().compareTo(e2.getValue());
+	                return res != 0 ? res : 1;
+	            }
+	        }
+	    );
+	    sortedEntries.addAll(map.entrySet());
+	    return sortedEntries;
+	}	
 	
 	/**
 	 * Calculate travel distance
